a8c867e91f6df1e7d7f4a68b528318f29017a621
Rostislav <rostislav.khantil@gmail.com>
2017-03-06 13:03:02 -0800

    Start. Installed Composer

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rostislav
+ * Date: 3/3/17
+ * Time: 7:17 AM
+ */
+include __DIR__ . '/../vendor/autoload.php';
+
+$dsn = getenv('MYSQL_DSN');
+$user = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+if (!isset($dsn, $user) || false === $password) {
+    echo('Set MYSQL_DSN, MYSQL_USER, and MYSQL_PASSWORD environment variables');
+}
+try {
+    $db = new PDO($dsn, $user, $password);
+
+} catch (PDOException $e) {
+    var_dump($e->getMessage() . '<br>');
+    var_dump($e->getFile() . '<br>');
+    var_dump($e->getLine() . '<br>');
+    var_dump($e->getPrevious() . '<br>');
+    var_dump($e->errorInfo . '<br>');
+    var_dump($e->getCode() . '<br>');
+    var_dump($e->getTraceAsString());
+}
+
+$smtm = $db->query('DROP TABLE example');
+
+//echo phpinfo();
